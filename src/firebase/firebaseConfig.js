dfffcf681d220cf17f7d5cc442349c6cb9247ae5
Muzaffarfullstack <amity.muzaffar@gmail.com>
2025-08-30 01:36:30 +0500

    auth fixed

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDY0_NYfJ9LuHkaadQCiB9Ut6RcYvlKlVA",
-  authDomain: "linguacraft-adfd6.firebaseapp.com",
-  projectId: "linguacraft-adfd6",
-  storageBucket: "linguacraft-adfd6.firebasestorage.app",
-  messagingSenderId: "97177170180",
-  appId: "1:97177170180:web:6de12dec0fa41751fd6051",
-  measurementId: "G-NQPD94MT2Z",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
